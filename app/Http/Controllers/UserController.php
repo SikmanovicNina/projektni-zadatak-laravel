485544a0d8c34fa18494e06ec23803ce1b813971
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-09-25 16:06:11 +0200

    Delete user
    https://matijabojanic.atlassian.net/browse/OL2S-36

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,8 +52,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json( 204);
     }
 }
