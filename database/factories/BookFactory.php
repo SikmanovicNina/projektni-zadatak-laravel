800b77344a2f6a062ce10c386fdce784cddf8c5c
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-10-08 13:31:47 +0200

    Add book factory
    https://matijabojanic.atlassian.net/browse/OL2S-17

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -17,7 +17,15 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->sentence(),
+            'number_of_pages' => $this->faker->numberBetween(100, 1000),
+            'number_of_copies' => $this->faker->numberBetween(1, 100),
+            'isbn' => $this->faker->isbn13(),
+            'language' => $this->faker->randomElement(['English', 'Spanish', 'French', 'German', 'Japanese']),
+            'binding' => $this->faker->randomElement(['Hardcover', 'Paperback', 'Spiral-bound']),
+            'script' => $this->faker->randomElement(['Cyrillic', 'Latin', 'Arabic']),
+            'dimensions' => $this->faker->randomElement(['21cm x 29.7cm', '15cm x 21cm', 'A1', 'A2', 'A3']),
         ];
     }
 }
