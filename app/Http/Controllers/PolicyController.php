7407c232e47b215519ad3916912585ae809b497f
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-10-10 08:58:04 +0200

    Policy view and editing
    https://matijabojanic.atlassian.net/browse/OL2S-14

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PolicyResource;
+use App\Models\Policy;
 use Illuminate\Http\Request;
 
 class PolicyController extends Controller
@@ -11,14 +13,28 @@ class PolicyController extends Controller
      */
     public function index()
     {
-        //
+        $policies = Policy::all();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => PolicyResource::collection($policies)
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Policy $policy)
     {
-        //
+        $validatedData = $request->validate([
+            'period' => ['required', 'integer', 'min:1'],
+        ]);
+
+        $policy->update($validatedData);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => new PolicyResource($policy)
+        ]);
     }
 }
