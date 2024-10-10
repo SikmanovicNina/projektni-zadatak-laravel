98c795a23e941d9393d6089238181d4c7adb06cf
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-10-10 09:00:49 +0200

    Add comments
    https://matijabojanic.atlassian.net/browse/OL2S-14

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PolicyResource;
 use App\Models\Policy;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PolicyController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     *  Display a listing of the resource.
+     *
+     * @return JsonResponse
      */
     public function index()
     {
@@ -21,8 +24,13 @@ class PolicyController extends Controller
         ]);
     }
 
+
     /**
-     * Update the specified resource in storage.
+     *  Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Policy $policy
+     * @return JsonResponse
      */
     public function update(Request $request, Policy $policy)
     {
