56b0c1ad151d3f749ba00dca0cc41be75b953489
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-10-24 10:56:33 +0200

    Fixes
    https://matijabojanic.atlassian.net/browse/OL2S-73

--- app/Console/Commands/ImportBooks.php
+++ app/Console/Commands/ImportBooks.php
@@ -7,7 +7,9 @@ use App\Mail\BooksFetchedMail;
 use App\Services\BookService;
 use Illuminate\Bus\Batch;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Throwable;
 
@@ -36,22 +38,30 @@ class ImportBooks extends Command
     }
 
     /**
-     * Handle the execution of the command to import books.
+     * Handle the book fetching and importing process.
      *
-     * Workflow:
-     * - Generates a random letter (A-Z) to fetch books based on the letter.
-     * - Calls the BookService to fetch books data from the Google Books API using the generated letter.
-     * - Checks if there are valid items in the API response.
-     * - For each book item:
-     *   - Extracts the volume information, including the ISBN and authors.
-     *   - If either ISBN or authors is missing, a warning is issued and the book is skipped.
-     *   - If valid data is present, a job (ImportBookJob) is dispatched to handle the import of the book asynchronously.
+     * This function:
+     * 1. Generates a random letter between A and Z.
+     * 2. Fetches books from an external API using the random letter as a query.
+     * 3. Iterates through the fetched books, validates the necessary data (ISBN, authors, book ID),
+     *    and skips any books with incomplete information.
+     * 4. For each valid book, it creates a job to import the book data and adds it to a batch.
+     * 5. If there are valid jobs, it dispatches the batch for processing.
+     * 6. Once the batch is completed successfully, an email is sent to notify the user.
+     * 7. If an error occurs during the batch process, the error is handled in the `catch` block
+     *
+     * Key components:
+     * - `$randomLetter`: A randomly generated letter used to query the book API.
+     * - `$booksData`: Holds the response from the external API containing the book data.
+     * - `$jobs`: An array of jobs to import books that contain valid data.
+     * - `Bus::batch()`: Executes a batch of jobs for importing the books.
+     * - `Mail::to()`: Sends an email notification when the batch of jobs has been successfully processed.
      *
      * @return void
+     * @throws Throwable
      */
     public function handle()
     {
-        $this->info('Novi pocetak');
         $randomLetter = chr(rand(65, 90));
 
         $booksData = $this->bookService->fetchBooksByQuery($randomLetter);
@@ -76,10 +86,15 @@ class ImportBooks extends Command
             if (!empty($jobs)) {
                 Bus::batch($jobs)
                     ->then(function (Batch $batch) {
-                        Mail::to("testing@gmail.me")->send(new BooksFetchedMail());
+                        if ($email = optional(Auth::user())->email) {
+                            Mail::to($email)->send(new BooksFetchedMail());
+                        }
                     })
                     ->catch(function (Batch $batch, Throwable $e) {
-                        // Error handling callback
+                        Log::error('Batch failed: ' . $e->getMessage(), [
+                            'batch_id' => $batch->id,
+                            'failed_jobs' => $batch->failedJobs,
+                        ]);
                     })
                     ->dispatch();
             }
