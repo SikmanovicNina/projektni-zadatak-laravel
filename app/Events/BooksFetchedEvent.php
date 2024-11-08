3bcee4e7a13f52f0510dab72a07baf874174f57f
NinaSikmanovic <sikmanovic.nikolina15@gmail.com>
2024-11-08 12:14:50 +0100

    Using reverb

---
+++ app/Events/BooksFetchedEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class BooksFetchedEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public function __construct($message)
+    {
+        $this->message = $message;
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => $this->message,
+        ];
+    }
+    public function broadcastOn(): array
+    {
+        Log::info("broadcast on");
+
+        return [
+            new Channel('booksChannel'),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    event(new \App\Events\BooksFetchedEvent('hello from Web.php', 1));
+
     return view('welcome');
 });
